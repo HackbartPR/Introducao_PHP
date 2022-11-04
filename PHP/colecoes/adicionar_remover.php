c30d90cab9dad1ae0ff1bf372f6f8a9030acede2
HackbartPR <cgharckbart@gmail.com>
2022-11-03 22:32:05 -0300

    enviar

---
+++ PHP/colecoes/adicionar_remover.php
@@ -0,0 +1,15 @@
+<?php
+
+$alunos2021 = ["Vinicius", "João", "Ana", "Roberto", "Maria"];
+
+//ADICIONA UMA LISTA DE VALORES NO FINAL DO ARRAY
+array_push($alunos2021, 'Carlos', 'Lais', 'Matheus');
+
+//ADICIONA UM ÚNICO ELEMENTO NO FINAL DO ARRAY
+$alunos2021[] = 'Gorete';
+
+var_dump($alunos2021);
+
+
+
+
